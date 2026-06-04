08d37cafe874077d0f85bf285c90c424e967d126
Jhojan Infante <jhojaninfante@gmail.com>
2026-06-04 12:49:05 -0500

    Estilos modificados

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,9 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/* Dark mode de Ionic */
+import '@ionic/react/css/palettes/dark.always.css';
+
 /* Tema personalizado */
 import './theme/variables.css';
 
